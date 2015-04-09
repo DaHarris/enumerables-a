4ed7eae52e59e714ac3577b01acc66ec9aa63fd4
Daniel Harris <bobth3bum@gmail.com>
2015-04-09 11:23:03 -0600

    find if an array has an empty value

---
+++ 10_any_empty.rb
@@ -0,0 +1,31 @@
+def any_empty?(words)
+  return true if words.include?('') || words.include?(nil)
+  false
+end
+# ------ code above this line ------
+
+require 'rspec/autorun'
+
+RSpec.describe '#any_empty?' do
+  it 'returns true if there are empty strings anywhere in the array' do
+    input = ['a', '', 'b']
+
+    expect(any_empty?(input)).to eq(true)
+  end
+
+  it 'returns true if there are nils anywhere in the array' do
+    input = ['a', nil, 'b']
+
+    expect(any_empty?(input)).to eq(true)
+  end
+
+  it 'returns false if all values are non-empty' do
+    input = ['fa', 'la']
+
+    expect(any_empty?(input)).to eq(false)
+  end
+
+  it 'returns false when given an empty array' do
+    expect(any_empty?([])).to eq(false)
+  end
+end
