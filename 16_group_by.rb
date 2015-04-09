af7994d714b0d677e4607dce8885c432b45b4985
Daniel Harris <bobth3bum@gmail.com>
2015-04-09 11:25:25 -0600

    group hash key value pairs by key or value specified by user

---
+++ 16_group_by.rb
@@ -0,0 +1,52 @@
+def group_by(input, value)
+  return_hash = {}
+  input.each do |hash|
+    if return_hash.has_key?(hash[value])
+      return_hash[hash[value]] << hash
+    else
+      return_hash[hash[value]] = [hash]
+    end
+  end
+  return return_hash
+end
+# ------ code above this line ------
+
+require 'rspec/autorun'
+
+RSpec.describe '#group_by' do
+  it 'returns a hash where the values are arrays of items, grouped by the given key' do
+    input = [
+      {x: 1, y: 6},
+      {x: 5, y: 2},
+      {x: 5, y: 6},
+    ]
+
+    expect(group_by(input, :x)).to eq({
+      1 => [ {x: 1, y: 6} ],
+      5 => [
+        {x: 5, y: 2},
+        {x: 5, y: 6},
+      ],
+    })
+
+    expect(group_by(input, :y)).to eq({
+      2 => [
+        {x: 5, y: 2},
+      ],
+      6 => [
+        {x: 1, y: 6},
+        {x: 5, y: 6},
+      ],
+    })
+  end
+
+  it 'returns an empty hash when given an empty array' do
+    expect(group_by([], :foo)).to eq({})
+  end
+
+  it 'returns nil as a key when given a hash that does not contain the given key' do
+    input = [ {y: 5} ]
+
+    expect(group_by(input, :x)).to eq({ nil => [ {y: 5} ] })
+  end
+end
