cf77210fd7ed0d0978af996f5b42a71db04ebc5e
Daniel Harris <bobth3bum@gmail.com>
2015-04-09 11:22:48 -0600

    select shortest word in array

---
+++ 09_shortest_word.rb
@@ -0,0 +1,24 @@
+def shortest_word(words)
+  words.inject(words[0]) {|shortest, word| shortest.size > word.size ? shortest = word : shortest}
+end
+# ------ code above this line ------
+
+require 'rspec/autorun'
+
+RSpec.describe '#shortest_word' do
+  it 'returns the shortest word in an array' do
+    input = ['a', 'big', 'black', 'bear', 'is', 'running']
+
+    expect(shortest_word(input)).to eq('a')
+  end
+
+  it 'returns the first shortest word if there are multiples' do
+    input = ['fa', 'la']
+
+    expect(shortest_word(input)).to eq('fa')
+  end
+
+  it 'returns nil when given an empty array' do
+    expect(shortest_word([])).to eq(nil)
+  end
+end
