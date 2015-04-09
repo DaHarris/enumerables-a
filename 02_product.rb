f525e9e23d03ddc75b52bcbbe5742116d42c3b1a
Daniel Harris <bobth3bum@gmail.com>
2015-04-09 11:25:41 -0600

    concatenate array of strings

---
+++ 02_product.rb
@@ -0,0 +1,19 @@
+def product(numbers)
+  numbers.inject{|sum, x| sum *= x}
+end
+
+# ------ code above this line ------
+
+require 'rspec/autorun'
+
+RSpec.describe '#product' do
+  it 'multiplies all of the numbers in the array' do
+    expect(product([1,2,7])).to eq(14)
+    expect(product([10])).to eq(10)
+    expect(product([1,2,-3])).to eq(-6)
+  end
+
+  it 'returns nil for empty arrays' do
+    expect(product([])).to eq(nil)
+  end
+end
