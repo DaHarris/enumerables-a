f789136bf366cd34b326ec4a427d2f7a47ebc093
Daniel Harris <bobth3bum@gmail.com>
2015-04-09 11:22:19 -0600

    return array of words in array shorter than a given amount

---
+++ 07_select_words_shorter_than.rb
@@ -0,0 +1,20 @@
+def select_words_shorter_than(words, length)
+  words.inject([]) {|sum, word| word.size < length ? sum << word : sum }
+end
+
+# ------ code above this line ------
+
+require 'rspec/autorun'
+
+RSpec.describe '#select_words_shorter_than' do
+  it 'returns an array of words shorter than the given length' do
+    input = ['a', 'big', 'black', 'bear', 'is', 'running']
+
+    expect(select_words_shorter_than(input, 3)).to eq(['a', 'is'])
+    expect(select_words_shorter_than(input, 4)).to eq(['a', 'big', 'is'])
+  end
+
+  it 'returns an empty array when given an empty array' do
+    expect(select_words_shorter_than([], 0)).to eq([])
+  end
+end
