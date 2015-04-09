7ce2e5228a8982c0b7e66bb0de676f5adcd40674
Daniel Harris <bobth3bum@gmail.com>
2015-04-09 11:20:49 -0600

    find minimum from array

---
+++ 03_min.rb
@@ -0,0 +1,19 @@
+def min(numbers)
+  return nil if numbers.count == 0
+  numbers.min
+end
+
+# ------ code above this line ------
+
+require 'rspec/autorun'
+
+RSpec.describe '#min' do
+  it 'returns the lowest number in the array' do
+    expect(min([7,5,12])).to eq(5)
+    expect(min([10])).to eq(10)
+  end
+
+  it 'returns nil for empty arrays' do
+    expect(min([])).to eq(nil)
+  end
+end
